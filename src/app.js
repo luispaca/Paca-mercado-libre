3d1b34ddae959fce07d63735da118de530615c63
lmpaca2208 <luispaca@uca.edu.ar>
2023-08-19 12:55:01 -0300

    updating

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname,'../public')));
 
-
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"views/home.html"));
 });
