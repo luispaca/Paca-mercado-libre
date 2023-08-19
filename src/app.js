7e18861942cba6c71e296f9e5be07dccbc149423
lmpaca2208 <luispaca@uca.edu.ar>
2023-08-19 20:30:46 -0300

    new updates

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ app.use(express.static(path.join(__dirname,'../public')));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"views/home.html"));
 });
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,"views/register.html"));
+});
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,"views/login.html"));
+});
 app.listen(3000,()=>{
     console.log('Levantando el server');
 });
